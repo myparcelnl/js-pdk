0f98a0350bdeaaf97d017f6caead0d6f5a1dd17b
Edie Lemoine <edie@myparcel.nl>
2023-02-03 11:24:48 +0100

    refactor(demo): fix ts error

--- apps/admin-demo/src/views/Settings.vue
+++ apps/admin-demo/src/views/Settings.vue
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 import {defineComponent, nextTick} from 'vue';
+import {PdkAdminComponent} from '@myparcel-pdk/common';
 import {useGlobalPdkAdmin} from '@myparcel-pdk/frontend-core';
 
 export default defineComponent({
@@ -12,7 +13,7 @@ export default defineComponent({
   setup: () => {
     const fe = useGlobalPdkAdmin();
 
-    void nextTick().then(() => fe.render('PluginSettings', '#pdk-PluginSettings'));
+    void nextTick().then(() => fe.render(PdkAdminComponent.PLUGIN_SETTINGS, '#pdk-PluginSettings'));
   },
 });
 </script>
