577f5f5f4e0716717f76702c6f4b6a98b3bdb8bd
Edie Lemoine <edie@myparcel.nl>
2023-02-15 15:41:45 +0100

    feat: add app for frontend delivery options

--- apps/checkout/src/index.ts
+++ apps/checkout/src/index.ts
@@ -1,1 +1,10 @@
 export type {FrontendEndpoint, FrontendPdkEndpointObject} from '@myparcel-pdk/common/src';
+
+export {
+  EVENT_HIDE_DELIVERY_OPTIONS,
+  EVENT_SHOW_DELIVERY_OPTIONS,
+  EVENT_UPDATED_ADDRESS,
+  EVENT_UPDATED_DELIVERY_OPTIONS,
+  EVENT_UPDATE_CONFIG,
+  EVENT_UPDATE_DELIVERY_OPTIONS,
+} from '@myparcel-pdk/frontend-delivery-options/src';
