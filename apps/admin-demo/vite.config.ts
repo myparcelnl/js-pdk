1027b47c71c688d4565889e7019166f6e9032db8
Edie Lemoine <edie@myparcel.nl>
2023-02-23 13:41:15 +0100

    fix(demo): fix duplicated symbols causing forms to break

--- apps/admin-demo/vite.config.ts
+++ apps/admin-demo/vite.config.ts
@@ -4,31 +4,40 @@ import vue from '@vitejs/plugin-vue';
 
 export const PORT = 9420;
 
-export default defineConfig((env) => ({
-  plugins: [vue()],
+export default defineConfig((env) => {
+  const isProd = env.mode === 'production';
 
-  server: {
-    port: PORT,
-    base: env.mode === 'production' ? '/js-pdk/' : '/',
-  },
+  return {
+    plugins: [vue()],
 
-  build: {
-    rollupOptions: {
-      external: ['vue', 'vue-router'],
-      plugins: [visualizer({filename: 'dist/stats.html'})],
+    server: {
+      port: PORT,
+      base: isProd ? '/js-pdk/' : '/',
     },
-  },
 
-  define: {
-    'process.env': {},
-    'process.argv': [],
-  },
-
-  test: {
-    environment: 'happy-dom',
-    coverage: {
-      enabled: false,
-      reporter: ['text', 'clover'],
+    build: {
+      rollupOptions: {
+        external: ['vue', 'vue-router'],
+        plugins: [visualizer({filename: 'dist/stats.html'})],
+      },
     },
-  },
-}));
+
+    optimizeDeps: {
+      // Optimizing this dependency causes the element and form injection keys to be mismatched.
+      exclude: ['@myparcel/vue-form-builder'],
+    },
+
+    define: {
+      'process.env': {},
+      'process.argv': [],
+    },
+
+    test: {
+      environment: 'happy-dom',
+      coverage: {
+        enabled: false,
+        reporter: ['text', 'clover'],
+      },
+    },
+  };
+});
