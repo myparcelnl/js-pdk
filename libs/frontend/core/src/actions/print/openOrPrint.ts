ba61df2069b2d12c09df21d246e8798983024c14
Edie Lemoine <edie@myparcel.nl>
2023-03-14 09:59:44 +0100

    fix: start on open or print action

--- libs/frontend/core/src/actions/print/openOrPrint.ts
+++ libs/frontend/core/src/actions/print/openOrPrint.ts
@@ -1,11 +1,12 @@
 import {downloadPdf, openPdfInNewWindow} from '../../services';
 import {ActionContextWithResponse} from '../executors';
+import {LABEL_OUTPUT_DOWNLOAD} from '../../data';
 import {PrintAction} from '../../types';
 import {generateLabelFilename} from '../../utils';
 
 export const openOrPrint = async <A extends PrintAction>(context: ActionContextWithResponse<A>): Promise<void> => {
   // @ts-expect-error todo
-  if (context.parameters.open === 'download') {
+  if (context.parameters.open === LABEL_OUTPUT_DOWNLOAD) {
     // @ts-expect-error todo
     await downloadPdf(context.response.url, generateLabelFilename(context.parameters));
     return;
