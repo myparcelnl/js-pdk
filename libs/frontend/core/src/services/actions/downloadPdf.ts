c08519da4de680e2346b793e9d4a005a60f52fc9
Edie Lemoine <edie@myparcel.nl>
2023-03-14 17:40:21 +0100

    fix(admin): fix downloading labels

--- libs/frontend/core/src/services/actions/downloadPdf.ts
+++ libs/frontend/core/src/services/actions/downloadPdf.ts
@@ -1,11 +1,8 @@
 /**
  * Try to get the pdf url. An error means the pdf file is not ready yet. Retry until it is, then download the label.
  */
-export async function downloadPdf(pdf: string, filename: string): Promise<void> {
+export async function downloadPdf(url: string, filename: string): Promise<void> {
   try {
-    const response = await fetch(pdf);
-    const blob = await response.blob();
-    const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
 
     link.href = url;
@@ -18,6 +15,6 @@ export async function downloadPdf(pdf: string, filename: string): Promise<void> 
       setTimeout(resolve, 100);
     });
 
-    return downloadPdf(pdf, filename);
+    return downloadPdf(url, filename);
   }
 }
