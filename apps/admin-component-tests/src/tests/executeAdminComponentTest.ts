e873c642f59dc239ac116f574a00a126fbe2e484
Edie Lemoine <edie@myparcel.nl>
2023-03-15 11:16:03 +0100

    test: fix ts error on admin instance

--- apps/admin-component-tests/src/tests/executeAdminComponentTest.ts
+++ apps/admin-component-tests/src/tests/executeAdminComponentTest.ts
@@ -1,10 +1,10 @@
 /* eslint-disable no-magic-numbers */
+import {AdminComponentName, AdminView} from '@myparcel-pdk/common/src';
 import {AdminInstance, createAdminConfig, createLogger} from '@myparcel-pdk/frontend-core/src';
 import {afterAll, beforeAll, describe, vi} from 'vitest';
-import {AdminComponentName} from '@myparcel-pdk/common/src';
 import {Component} from 'vue';
-import {testMap} from './testMap';
 import {QueryKey} from '@tanstack/vue-query';
+import {testMap} from './testMap';
 
 export const executeAdminComponentTest = (name: AdminComponentName, component: Omit<Component, 'props'>): void => {
   const test = testMap[name];
@@ -31,9 +31,10 @@ export const executeAdminComponentTest = (name: AdminComponentName, component: O
       vi.mock('../usePdkInstance', () => ({
         usePdkInstance: (): AdminInstance => ({
           appName: 'test',
-          context: {},
           config: createAdminConfig(),
+          context: {},
           logger: createLogger('test'),
+          view: 'test' as AdminView,
         }),
       }));
     });
