072f744e4004efd0f4e90af247424cd5e790d303
Edie Lemoine <edie@myparcel.nl>
2023-04-18 11:01:12 +0200

    fix(admin-preset-default): remove invalid prop from row

--- apps/admin-preset-default/src/components/DefaultRow.vue
+++ apps/admin-preset-default/src/components/DefaultRow.vue
@@ -7,12 +7,6 @@
 <script lang="ts" setup>
 defineProps<{
   /**
-   * The number of columns to span.
-   */
-  // eslint-disable-next-line vue/no-unused-properties
-  span?: number;
-
-  /**
    * The width of the row.
    */
   // eslint-disable-next-line vue/no-unused-properties
