d80dc409c59ae8ce055a0ca7adb9e555ab9f0711
Edie Lemoine <edie@myparcel.nl>
2023-04-20 17:42:55 +0200

    refactor: fix imports

--- apps/checkout/src/index.ts
+++ apps/checkout/src/index.ts
@@ -5,8 +5,8 @@ export * from '@myparcel-pdk/frontend-checkout-core/src';
 export {
   PdkDeliveryOptionsEvent,
   initializeCheckoutDeliveryOptions,
-} from '@myparcel-pdk/frontend-checkout-delivery-options';
+} from '@myparcel-pdk/frontend-checkout-delivery-options/src';
 
-export {initializeCheckoutSeparateAddressFields} from '@myparcel-pdk/frontend-checkout-separate-address-fields';
+export {initializeCheckoutSeparateAddressFields} from '@myparcel-pdk/frontend-checkout-separate-address-fields/src';
 
-export {initializeCheckoutTaxFields} from '@myparcel-pdk/frontend-checkout-tax-fields';
+export {initializeCheckoutTaxFields} from '@myparcel-pdk/frontend-checkout-tax-fields/src';
