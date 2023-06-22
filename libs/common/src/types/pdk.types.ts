5d06d9bd7e520c2daaab43c233787e0c33b288b7
markernst97 <markiervc@gmail.com>
2023-06-22 16:01:27 +0200

    Revert "feat: add tristate component to admin component types"
    This reverts commit b80ca0af03f2fa1e8b9e51ace5bb6e60f9e861bb.

--- libs/common/src/types/pdk.types.ts
+++ libs/common/src/types/pdk.types.ts
@@ -42,7 +42,6 @@ export enum AdminComponent {
   TextInput = 'TextInput',
   TimeInput = 'TimeInput',
   ToggleInput = 'ToggleInput',
-  TristateInput = 'TristateInput',
 }
 
 export const requiredAdminComponentNames = [
@@ -75,7 +74,6 @@ export const requiredAdminComponentNames = [
   AdminComponent.TextInput,
   AdminComponent.TimeInput,
   AdminComponent.ToggleInput,
-  AdminComponent.TristateInput,
 ] as const;
 
 export const optionalAdminPlainWrapperComponentNames = [
