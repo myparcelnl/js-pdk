6ad5cf76f8395755734e1d2ced99558da9b5026e
Edie Lemoine <edie@myparcel.nl>
2023-07-03 11:16:33 +0200

    fix(admin-core): fix tri-state component not being readonly initially

--- libs/frontend/admin-core/src/composables/useTriStateInputContext.ts
+++ libs/frontend/admin-core/src/composables/useTriStateInputContext.ts
@@ -1,4 +1,4 @@
-import {type Component, computed, type WritableComputedRef} from 'vue';
+import {type Component, computed, onMounted, type WritableComputedRef} from 'vue';
 import {get} from '@vueuse/core';
 import {type ComponentOrHtmlElement, type InteractiveElementInstance, useForm} from '@myparcel/vue-form-builder';
 import {createFormElement} from '../utils';
@@ -25,7 +25,14 @@ const TRI_STATE_VALUE_DEFAULT = -1;
  * A tri-state input can hold a value of 0, 1 or -1. -1 is used to indicate that the value should be inherited or defaulted.
  */
 export const useTriStateInputContext: UseTriStateInputContext = (props, emit) => {
+  const form = useForm();
   const {translate} = useLanguage();
+
+  const setToggleReadOnly = (value: boolean): void => {
+    const toggle = form.getField<InteractiveElementInstance<Component, string, boolean>>(props.element.name);
+
+    toggle.setReadOnly(value);
+  };
 
   const inheritValueElement = createFormElement<boolean>({
     label: translate('settings_use_default_value'),
@@ -49,20 +56,20 @@ export const useTriStateInputContext: UseTriStateInputContext = (props, emit) =>
     },
   });
 
-  const form = useForm();
-
   const inheritValueModel = computed<boolean>({
     set(value) {
-      const toggle = form.getField<InteractiveElementInstance<Component, string, boolean>>(props.element.name);
-
       model.value = value ? TRI_STATE_VALUE_DEFAULT : (Number(toggleModel.value) as 0 | 1);
 
-      toggle.setReadOnly(value);
+      setToggleReadOnly(value);
     },
 
     get() {
       return get(props.element.ref) === TRI_STATE_VALUE_DEFAULT;
     },
+  });
+
+  onMounted(() => {
+    setToggleReadOnly(get(inheritValueModel));
   });
 
   return {
