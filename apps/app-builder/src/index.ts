0cfd44ea43b8eb47451e5b9ca010348b3a70edad
Edie Lemoine <edie@myparcel.nl>
2023-07-07 10:52:29 +0200

    fix(app-builder): export PdkBuilderConfig type

--- apps/app-builder/src/index.ts
+++ apps/app-builder/src/index.ts
@@ -2,6 +2,8 @@
 
 import {type PdkBuilderConfig} from './types';
 
+export type {PdkBuilderConfig};
+
 export {PdkPlatformName} from './types';
 
 export const defineConfig = <C extends PdkBuilderConfig | (() => PdkBuilderConfig)>(config: C): C => config;
