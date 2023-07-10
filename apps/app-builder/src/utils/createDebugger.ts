da9f0c5dc975b144194da9c43ad9832c11cb86b2
Edie Lemoine <edie@myparcel.nl>
2023-07-10 10:58:53 +0200

    fix(app-builder): fix debugger being quiet when quiet is not passed

--- apps/app-builder/src/utils/createDebugger.ts
+++ apps/app-builder/src/utils/createDebugger.ts
@@ -4,7 +4,7 @@ import {type CommandArgs} from '../types';
 export const createDebugger = (name: string, args: CommandArgs): Debugger => {
   const debug = debugFactory(`@myparcel-pdk/app-builder:${name}`);
 
-  debug.enabled = args.quiet === false;
+  debug.enabled = args.quiet !== false;
 
   // eslint-disable-next-line no-console
   debug.log = console.log.bind(console);
