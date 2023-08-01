2491b2bc0fa5a925efedc4586f3d0c5d924dd735
Edie Lemoine <edie@myparcel.nl>
2023-08-01 11:55:46 +0200

    refactor: remove unused code

--- libs/frontend/admin-core/src/sdk/composables/api/index.ts
+++ libs/frontend/admin-core/src/sdk/composables/api/index.ts
@@ -1,2 +1,1 @@
 export * from './useCarrier';
-export * from './useCarriers';
--- libs/frontend/admin-core/src/sdk/composables/api/useCarrier.ts
+++ libs/frontend/admin-core/src/sdk/composables/api/useCarrier.ts
@@ -3,11 +3,10 @@ import {useQuery, useQueryClient} from '@tanstack/vue-query';
 import {type EndpointResponse, type GetCarrier} from '@myparcel/sdk';
 import {type CarrierName} from '@myparcel/constants';
 import {useMyParcelApi} from '../useMyParcelApi';
-import {QUERY_KEY_CARRIERS} from './useCarriers';
 
-export const useCarrier = (carrier?: CarrierName) => {
+export const useCarrier = (carrier: CarrierName) => {
   const queryClient = useQueryClient();
-  const queryKey = [QUERY_KEY_CARRIERS, ...(carrier ? [carrier] : [])] as const;
+  const queryKey = ['carriers', {name: carrier}] as const;
 
   return useQuery<EndpointResponse<GetCarrier>[number]>(
     queryKey,
