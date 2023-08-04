eea8c9a13f2a9121b0c27739d27399e470bbad87
Edie Lemoine <edie@myparcel.nl>
2023-08-04 10:14:35 +0200

    fix(app-builder): improve regex version replacing

--- apps/app-builder/src/increment/replaceVersionByRegex.ts
+++ apps/app-builder/src/increment/replaceVersionByRegex.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-import {REGEX_VERSION, VerbosityLevel} from '../constants';
+import {VerbosityLevel} from '../constants';
 import {type RegexVersionSource, type VersionReplacer} from './types';
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
@@ -7,19 +7,23 @@ export const replaceVersionByRegex: VersionReplacer<RegexVersionSource> = (
   {match, contents, newVersion},
   {debug, args},
 ) => {
-  const regExp = RegExp(match.regex);
+  const regExp = match.regex instanceof RegExp ? match.regex : new RegExp(match.regex, 'g');
   const results = regExp.exec(contents);
   const matchedVersion = results?.[1];
 
+  if (args.verbose >= VerbosityLevel.VeryVeryVerbose) {
+    debug('– Found %s in %s', chalk.cyanBright(matchedVersion), chalk.cyanBright(match.path));
+  }
+
   if (matchedVersion) {
     const foundString = results?.[0] ?? '';
-    const replacement = foundString.replace(REGEX_VERSION, newVersion);
+    const newString = foundString.replace(matchedVersion, newVersion);
 
     if (args.verbose >= VerbosityLevel.VeryVerbose) {
-      debug('– Replacing %s with %s', chalk.redBright(foundString), chalk.greenBright(replacement));
+      debug('– Replacing %s with %s', chalk.redBright(foundString), chalk.greenBright(newString));
     }
 
-    contents = contents.replace(foundString, replacement);
+    contents = contents.replace(foundString, newString);
   } else if (args.verbose >= VerbosityLevel.VeryVerbose) {
     debug(chalk.redBright('– No version found in %s for key %s'), chalk.cyanBright(match.path));
   }
