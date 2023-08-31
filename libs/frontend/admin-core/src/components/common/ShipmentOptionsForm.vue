133ab3f05d0e2c270ea9f63f981699c1e75b08e4
Edie Lemoine <edie@myparcel.nl>
2023-08-31 12:45:15 +0200

    refactor: remove test data

--- libs/frontend/admin-core/src/components/common/ShipmentOptionsForm.vue
+++ libs/frontend/admin-core/src/components/common/ShipmentOptionsForm.vue
@@ -1,19 +1,10 @@
 <template>
-  <div>
-    <div>
-      <MagicForm
-        v-test="$.type.__name"
-        :form="shipmentOptionsForm" />
-    </div>
-
-    <div>
-      <pre v-text="values"></pre>
-    </div>
-  </div>
+  <MagicForm
+    v-test="$.type.__name"
+    :form="shipmentOptionsForm" />
 </template>
 
 <script lang="ts" setup>
-import {computed} from 'vue';
 import {get, isDef} from '@vueuse/core';
 import {MagicForm} from '@myparcel/vue-form-builder';
 import {createShipmentOptionsForm} from '../../forms';
@@ -22,6 +13,4 @@ import {useOrdersData} from '../../composables';
 const queries = useOrdersData();
 
 const shipmentOptionsForm = createShipmentOptionsForm(queries.map((data) => get(data.order)).filter(isDef));
-
-const values = computed(() => shipmentOptionsForm.getValues());
 </script>
