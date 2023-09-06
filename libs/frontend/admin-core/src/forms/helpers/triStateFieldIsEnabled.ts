86d9eaa252aa66184f6102f311c000a952b6ad78
Edie Lemoine <edie@myparcel.nl>
2023-09-06 10:38:04 +0200

    refactor: fix type error

--- libs/frontend/admin-core/src/forms/helpers/triStateFieldIsEnabled.ts
+++ libs/frontend/admin-core/src/forms/helpers/triStateFieldIsEnabled.ts
@@ -1,9 +1,11 @@
 import {get} from '@vueuse/core';
 import {type FormInstance} from '@myparcel/vue-form-builder';
+import {type ElementInstance} from '../../types';
 import {TriState} from '../../data';
+import {type TriStateElementProps} from '../../composables';
 
 export const triStateFieldIsEnabled = (form: FormInstance, fieldName: string): boolean => {
-  const field = form.getField(fieldName);
+  const field = form.getField(fieldName) as ElementInstance<TriStateElementProps> | undefined;
 
   if (!field) {
     return false;
