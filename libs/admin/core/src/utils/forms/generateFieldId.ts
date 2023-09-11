aa568fd27363448b99ac7b42ff3477a2ddb5791b
Edie Lemoine <edie@myparcel.nl>
2023-09-11 15:41:05 +0200

    feat(admin): support propRefs in generateFieldId

--- libs/admin/core/src/utils/forms/generateFieldId.ts
+++ libs/admin/core/src/utils/forms/generateFieldId.ts
@@ -1,10 +1,12 @@
+import {unref} from 'vue';
+import {type MaybeRef} from '@vueuse/core';
 import {type InteractiveElementInstance} from '@myparcel/vue-form-builder';
 import {type ElementInstance} from '../../types';
 import {type FORM_KEYS, useAdminConfig, useElement} from '../../index';
 
-export const generateFieldId = (element?: ElementInstance): string => {
+export const generateFieldId = (element?: MaybeRef<ElementInstance>): string => {
   const config = useAdminConfig();
-  const resolvedElement = element ?? useElement();
+  const resolvedElement = unref(element) ?? useElement();
 
   const formBaseName = resolvedElement.form.name.split('--')[0] as (typeof FORM_KEYS)[number];
 
