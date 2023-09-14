87869bd3f1d5d4707ef373687ddb40965513521d
Edie Lemoine <edie@myparcel.nl>
2023-09-14 10:23:51 +0200

    fix(app-builder): expose more types

--- apps/app-builder/src/index.ts
+++ apps/app-builder/src/index.ts
@@ -2,6 +2,8 @@
 
 import {type PdkBuilderConfig} from './types';
 
+export type {PdkBuilderCommand, StringGenerator} from './types';
+
 export type {PdkBuilderConfig};
 
 export {PdkPlatformName} from './types';
