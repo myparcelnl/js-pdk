25821f98b46364b26add0f060178abb6b098913f
Edie Lemoine <edie@myparcel.nl>
2023-10-31 15:18:04 +0100

    fix(app-builder): skip incrementing files that do not exist

--- apps/app-builder/src/commands/increment.ts
+++ apps/app-builder/src/commands/increment.ts
@@ -1,9 +1,17 @@
-import path from 'path';
 import fs from 'fs';
 import glob from 'fast-glob';
 import chalk from 'chalk';
 import {isOfType} from '@myparcel/ts-utils';
-import {executePromises, getFileContents, logSourcePath, reportDryRun, resolveStrings, usesPhpScoper} from '../utils';
+import {
+  executePromises,
+  exists,
+  getFileContents,
+  logSourcePath,
+  reportDryRun,
+  resolvePath,
+  resolveStrings,
+  usesPhpScoper,
+} from '../utils';
 import {type PdkBuilderCommand} from '../types';
 import {
   type RegexVersionSource,
@@ -31,9 +39,7 @@ const increment: PdkBuilderCommand = async (context) => {
       sources.push(`${config.phpScoper.vendorOutDir}/${source.path}`);
     }
 
-    const strings = resolveStrings(context, sources);
-
-    return {source, files: glob.sync(strings, {cwd: env.cwd})};
+    return {source, files: glob.sync(resolveStrings(context, sources), {cwd: env.cwd})};
   });
 
   await executePromises(
@@ -41,7 +47,16 @@ const increment: PdkBuilderCommand = async (context) => {
     matches.map(async ({files, source: match}) => {
       return Promise.all(
         files.sort().map(async (file) => {
-          const filePath = path.resolve(env.cwd, file);
+          const filePath = resolvePath(file, context);
+
+          if (!(await exists(filePath))) {
+            if (context.args.verbose >= VerbosityLevel.VeryVerbose) {
+              debug('Skipping %s', logSourcePath(filePath, context));
+            }
+
+            return;
+          }
+
           const contents = await getFileContents(filePath);
 
           let output: VersionReplacerOutput;
@@ -51,14 +66,11 @@ const increment: PdkBuilderCommand = async (context) => {
           }
 
           if (isOfType<RegexVersionSource>(match, 'regex')) {
-            output = replaceVersionByRegex({match, contents, newVersion}, {config, args, debug});
+            output = replaceVersionByRegex({match, contents, newVersion}, context);
           } else if (filePath.endsWith('.json')) {
-            output = replaceVersionInJson({match, contents, newVersion}, {config, args, debug});
+            output = replaceVersionInJson({match, contents, newVersion}, context);
           } else {
-            output = replaceVersionByRegex(
-              {match: {...match, regex: REGEX_VERSION}, contents, newVersion},
-              {config, args, debug},
-            );
+            output = replaceVersionByRegex({match: {...match, regex: REGEX_VERSION}, contents, newVersion}, context);
           }
 
           if (!args.dryRun) {
