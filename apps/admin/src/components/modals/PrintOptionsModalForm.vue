fb12829a6a01b35bcb7fd1ca213e27f4953ac3ad
Edie Lemoine <edie@myparcel.nl>
2023-11-14 15:13:07 +0100

    fix(admin): fix print options modal not rendering correctly

--- apps/admin/src/components/modals/PrintOptionsModalForm.vue
+++ apps/admin/src/components/modals/PrintOptionsModalForm.vue
@@ -1,10 +1,12 @@
 <template>
   <MagicForm
+    v-if="form"
     v-test="$.type.__name"
     :form="form" />
 </template>
 
 <script lang="ts" setup>
+import {markRaw, onMounted, ref} from 'vue';
 import {defineForm, MagicForm} from '@myparcel/vue-form-builder';
 import {AdminModalKey} from '../../types';
 import {generateFormFields} from '../../forms';
@@ -14,12 +16,18 @@ const context = useContext();
 const pluginSettings = usePluginSettings();
 const config = useAdminConfig();
 
-const form = defineForm(AdminModalKey.PrintOptions, {
-  ...config.formConfigOverrides?.modal,
-  ...config.formConfigOverrides?.[AdminModalKey.PrintOptions],
-  fields: generateFormFields({
-    fields: context.printOptionsView.elements,
-    values: pluginSettings.label,
-  }),
+const form = ref();
+
+onMounted(() => {
+  form.value = markRaw(
+    defineForm(AdminModalKey.PrintOptions, {
+      ...config.formConfigOverrides?.modal,
+      ...config.formConfigOverrides?.[AdminModalKey.PrintOptions],
+      fields: generateFormFields({
+        fields: context.printOptionsView.elements,
+        values: pluginSettings.label,
+      }),
+    }),
+  );
 });
 </script>
