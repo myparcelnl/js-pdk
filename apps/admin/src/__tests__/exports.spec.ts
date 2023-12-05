d57e18cb1a36631bb2d8b95dd8574832a366b3f4
Edie Lemoine <edie@myparcel.nl>
2023-12-05 15:40:25 +0100

    test: update tests

--- apps/admin/src/__tests__/exports.spec.ts
+++ apps/admin/src/__tests__/exports.spec.ts
@@ -1,8 +1,13 @@
 import {describe, expect, it} from 'vitest';
+import * as testing from '../testing';
 import * as index from '../index';
 
 describe('exports', () => {
   it('exports from index.ts', () => {
     expect(Object.keys(index).sort()).toMatchSnapshot();
+  });
+
+  it('exports from testing.ts', () => {
+    expect(Object.keys(testing).sort()).toMatchSnapshot();
   });
 });
