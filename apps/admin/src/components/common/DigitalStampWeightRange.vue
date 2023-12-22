4d1c679c23f13e02057376ac9b34a24c1f2db413
markernst97 <markiervc@gmail.com>
2023-12-22 14:45:06 +0100

    fix: feedback

--- apps/admin/src/components/common/DigitalStampWeightRange.vue
+++ apps/admin/src/components/common/DigitalStampWeightRange.vue
@@ -1,9 +1,9 @@
 <template>
-  <span v-text="label" />
+  <span v-text="` ${currentRange.plainLabel}`" />
 </template>
 
 <script lang="ts" setup>
-import {toRefs, computed} from 'vue';
+import {toRefs} from 'vue';
 import {useDigitalStampRanges} from '../../composables';
 
 const props = defineProps<{weight: number}>();
@@ -11,7 +11,4 @@ const props = defineProps<{weight: number}>();
 const propRefs = toRefs(props);
 
 const {currentRange} = useDigitalStampRanges(propRefs.weight);
-const label = computed(() => {
-  return currentRange?.value?.plainLabel ? ` (${currentRange.value.plainLabel})` : '';
-});
 </script>
