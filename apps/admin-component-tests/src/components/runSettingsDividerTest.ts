3a01b62ee4ddc2b979777489b7e2ad49a5f27b09
Edie Lemoine <edie@myparcel.nl>
2024-03-13 16:12:48 +0100

    fix(admin-component-tests): fix settings divider test

--- apps/admin-component-tests/src/components/runSettingsDividerTest.ts
+++ apps/admin-component-tests/src/components/runSettingsDividerTest.ts
@@ -3,7 +3,12 @@ import {createInputOptions} from '../helpers';
 import {runCommonComponentTests} from '../common';
 
 export const runSettingsDividerTest = ((component) => {
-  const options = createInputOptions('text');
+  const options = createInputOptions('text', {
+    props: {
+      level: 2,
+      heading: 'test',
+    },
+  });
 
   runCommonComponentTests(component, options);
 }) satisfies AdminComponentTest;
