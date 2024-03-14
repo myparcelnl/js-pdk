fa87c3116358857e4a02cdf318bf02013a4bce1e
Edie Lemoine <edie@myparcel.nl>
2024-03-14 14:53:10 +0100

    fix(checkout): fix unnecessary updates with delivery options 6.x

--- libs/checkout-delivery-options/src/utils/deliveryOptionsIsRendered.ts
+++ libs/checkout-delivery-options/src/utils/deliveryOptionsIsRendered.ts
@@ -1,8 +1,14 @@
 import {PdkUtil, useConfig, useUtil} from '@myparcel-pdk/checkout-common';
 
+/**
+ * In delivery options 5.x, checking if the element exists is enough. In 6.x, the element is always present, so we need
+ * to check if it has content.
+ */
 export const deliveryOptionsIsRendered = (): boolean => {
   const config = useConfig();
   const getElement = useUtil(PdkUtil.GetElement);
 
-  return !getElement(config.selectors.deliveryOptions, false);
+  const element = getElement(config.selectors.deliveryOptions, false);
+
+  return !element || element.innerHTML !== '';
 };
