e36ce0a0450cfec6aac178bb09c0b94933d206c1
Edie Lemoine <edie@myparcel.nl>
2024-08-07 16:54:50 +0200

    fix(tests): install missing vtu wrapper plugin

--- apps/admin-component-tests/src/tests/executePdkComponentTests.ts
+++ apps/admin-component-tests/src/tests/executePdkComponentTests.ts
@@ -1,8 +1,12 @@
+import {config} from '@vue/test-utils';
 import {doComponentTestSetup} from '@myparcel-pdk/admin/testing';
 import {type AdminComponent, type AdminComponentMap} from '@myparcel-pdk/admin';
+import {wrapperPlugin} from '../helpers/wrapperPlugin';
 import {executeAdminComponentTest} from './executeAdminComponentTest';
 
 export const executePdkComponentTests = (components: Partial<AdminComponentMap> = {}): void => {
+  config.plugins.VueWrapper.install(wrapperPlugin);
+
   doComponentTestSetup(components);
 
   Object.entries(components).forEach(([name, component]) => {
