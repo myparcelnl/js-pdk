2e2447a4faf25045498aee64f0d5213b512ffaa0
Edie Lemoine <edie@myparcel.nl>
2024-10-02 16:58:18 +0200

    fix: fix downloading logs

--- apps/admin/src/services/print/openPdfInNewWindow.ts
+++ apps/admin/src/services/print/openPdfInNewWindow.ts
@@ -1,10 +1,11 @@
-import {openUrl} from '../../utils';
+import {openUrlInNewTab} from '../../utils';
 
 /**
  * Opens a new window with the given base64 encoded pdf.
  */
 export const openPdfInNewWindow = async (pdf: string): Promise<void> => {
   const blob = await (await fetch(`data:application/pdf;base64,${pdf}`)).blob();
+  const url = URL.createObjectURL(blob);
 
-  openUrl(URL.createObjectURL(blob));
+  openUrlInNewTab(url);
 };
