9ed1c0dec2f2963002413924956ad8291fce2628
Joeri van Veen <joeri@myparcel.nl>
2024-12-10 16:46:17 +0100

    fix(ordermode): show barcode when printed from backoffice
    INT-627

--- apps/admin/src/components/OrderListItem/OrderModeOrderListItem.vue
+++ apps/admin/src/components/OrderListItem/OrderModeOrderListItem.vue
@@ -1,5 +1,10 @@
 <template>
-  <DeliveryOptionsExcerpt />
+  <DeliveryOptionsExcerpt v-if="shipments.length === 0" />
+
+  <ShipmentLabel
+    v-for="shipment in data?.shipments ?? []"
+    :key="`${data?.externalIdentifier}_${shipment.id}`"
+    :shipment-id="shipment.id" />
 
   <PdkButtonGroup v-test="[$.type.__name, order?.externalIdentifier]">
     <template v-if="order?.exported">
@@ -18,13 +23,19 @@
 </template>
 
 <script lang="ts" setup>
+import {computed, toValue} from 'vue';
 import {Size} from '@myparcel-pdk/common';
 import {ActionButton, DeliveryOptionsExcerpt} from '../common';
 import {instantiateAction, instantiateActions} from '../../services';
 import {useOrderData} from '../../composables';
 import {orderExportAction, ordersEditAction, orderViewInBackofficeAction} from '../../actions';
+import ShipmentLabel from './ShipmentLabel.vue';
 
-const {order} = useOrderData();
+const {query, order} = useOrderData();
+
+const data = computed(() => toValue(query.data));
+
+const shipments = computed(() => toValue(data)?.shipments ?? []);
 
 const showExportedOrderAction = instantiateAction(orderViewInBackofficeAction);
 
