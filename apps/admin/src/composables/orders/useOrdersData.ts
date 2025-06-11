a98946bc5a67ad6c648b8b61c218dc7b205d9ae5
Freek van Rijt <freek.vanrijt@myparcel.nl>
2025-06-11 15:34:52 +0200

    fix(admin): fix modals sometimes exporting incorrect shipment (#233)
    fixes issues where the shipment options modal would partially retain
    stale data from an earlier instance and would not immediately open
    multiple times
    
    fixes INT-846

--- apps/admin/src/composables/orders/useOrdersData.ts
+++ apps/admin/src/composables/orders/useOrdersData.ts
@@ -3,7 +3,12 @@ import {getOrderId, validateId} from '../../utils';
 import {type UseOrderData, useOrderData} from './useOrderData';
 
 export const useOrdersData = (externalIdentifiers?: OneOrMore<string>): UseOrderData[] => {
-  const orderIds = validateId(externalIdentifiers ?? getOrderId(), true);
-
-  return toArray(orderIds).map((externalIdentifier) => useOrderData(externalIdentifier));
+  try {
+    const orderIds = validateId(externalIdentifiers ?? getOrderId(), true);
+    return toArray(orderIds).map((externalIdentifier) => useOrderData(externalIdentifier));
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.warn(error);
+    return [];
+  }
 };
--- apps/admin/src/views/Modals.vue
+++ apps/admin/src/views/Modals.vue
@@ -4,16 +4,13 @@
       <PrintOptionsModal v-if="pluginSettings.label.prompt" />
     </KeepAlive>
 
-    <KeepAlive>
-      <ShipmentOptionsModal v-if="modalOrder" />
-    </KeepAlive>
+    <ShipmentOptionsModal />
   </div>
 </template>
 
 <script lang="ts" setup>
-import {computed} from 'vue';
 import {useActionStore, useQueryStore} from '../stores';
-import {useModalOrder, usePluginSettings} from '../composables';
+import {usePluginSettings} from '../composables';
 import ShipmentOptionsModal from '../components/modals/ShipmentOptionsModal.vue';
 import PrintOptionsModal from '../components/modals/PrintOptionsModal.vue';
 
@@ -22,5 +19,4 @@ useQueryStore().registerContextQueries();
 useActionStore().registerModalActions();
 
 const pluginSettings = usePluginSettings();
-const modalOrder = computed(() => useModalOrder());
 </script>
