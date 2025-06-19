3dbeb3e11cb86aca80a1dbf590909062fedfe369
Jochem Gravendeel <119667571+GravendeelJochem@users.noreply.github.com>
2025-06-19 11:16:30 +0200

    fix(js-pdk): open correct NL or BE BackOffice URL based on platform (#241)
    INT-756

--- apps/admin/src/actions/definitions/orders.ts
+++ apps/admin/src/actions/definitions/orders.ts
@@ -8,6 +8,7 @@ import {type OrderIds} from '../../types';
 import {useModalStore} from '../../stores';
 import {AdminAction, AdminIcon, AdminModalKey} from '../../data';
 import {shipmentsUpdateAction} from './shipments';
+import {useGlobalContext} from '../../composables/context/useGlobalContext';
 
 /**
  * Open modal to edit order shipment options.
@@ -116,6 +117,12 @@ export const orderViewInBackofficeAction = defineAction({
   icon: AdminIcon.External,
   label: 'order_view_in_backoffice',
   handler() {
-    window.open('https://backoffice.myparcel.nl/orders', '_blank');
+    const {platform} = useGlobalContext();
+    const backofficeUrls = {
+      NL: 'https://backoffice.myparcel.nl/orders',
+      BE: 'https://backoffice.sendmyparcel.be/orders',
+    };
+    const url = backofficeUrls[platform.localCountry as 'NL' | 'BE'] || backofficeUrls['NL'];
+    window.open(url, '_blank');
   },
 });
