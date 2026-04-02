8147ccd1da4c6c40e28d4974670ea29ed80f0a34
Freek van Rijt <freek.vanrijt@myparcel.nl>
2026-04-02 15:30:44 +0200

    feat: apply carrier-keyed inter-field dependencies in createShipmentOptionField

--- apps/admin/src/forms/shipmentOptions/fields/createShipmentOptionField.ts
+++ apps/admin/src/forms/shipmentOptions/fields/createShipmentOptionField.ts
@@ -2,9 +2,13 @@ import {TriState} from '@myparcel-dev/pdk-common';
 import {type InteractiveElementConfiguration} from '@myparcel-dev/vue-form-builder';
 import {type ShipmentOptionsRefs} from '../types';
 import {type CarrierOptionData} from '../carrierOptionData.types';
+import {getFieldDependencies} from '../fieldDependencies';
 import {createHasShipmentOptionWatcher, defineFormField, getFieldLabel, resolveFormComponent} from '../../helpers';
+import {getFormCarrierName} from '../../helpers/getFormCarrierName';
 import {AdminComponent} from '../../../data';
 import {createRef} from './createRef';
+
+const SHIPMENT_OPTIONS_PREFIX = 'deliveryOptions.shipmentOptions';
 
 /**
  * Creates a generic shipment option field as a TriState toggle.
@@ -34,6 +38,53 @@ export const createShipmentOptionField = (
     label: getFieldLabel(name),
     visibleWhen: createHasShipmentOptionWatcher(name),
     disabledWhen: optionData.isRequired ? () => true : createHasShipmentOptionWatcher(name, true),
+
+    afterUpdate(field, value) {
+      const carrierName = getFormCarrierName(field.form);
+
+      if (!carrierName) {
+        return;
+      }
+
+      const deps = getFieldDependencies(carrierName, name);
+
+      if (!deps) {
+        return;
+      }
+
+      const isEnabled = TriState.On === value;
+
+      for (const requiredOption of deps.requires ?? []) {
+        const targetField = field.form.getField(`${SHIPMENT_OPTIONS_PREFIX}.${requiredOption}`);
+
+        if (!targetField) {
+          continue;
+        }
+
+        if (isEnabled) {
+          field.form.setValue(targetField.name, TriState.On);
+          targetField.props.readOnly = true;
+        } else {
+          targetField.props.readOnly = false;
+        }
+      }
+
+      for (const excludedOption of deps.excludes ?? []) {
+        const targetField = field.form.getField(`${SHIPMENT_OPTIONS_PREFIX}.${excludedOption}`);
+
+        if (!targetField) {
+          continue;
+        }
+
+        if (isEnabled) {
+          field.form.setValue(targetField.name, TriState.Off);
+          targetField.props.readOnly = true;
+        } else {
+          targetField.props.readOnly = false;
+        }
+      }
+    },
+
     ...config,
   });
 };
