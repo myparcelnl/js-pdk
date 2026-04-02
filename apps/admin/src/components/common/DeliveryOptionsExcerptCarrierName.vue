79ba11a2d8ebd397c6a1945f0f70c025652dfbb0
Freek van Rijt <freek.vanrijt@myparcel.nl>
2026-04-02 15:37:01 +0200

    feat: update carrier definition
    match carrier definition to new PDK approach based on contract definitions/capabilities.
    
    fixes INT-1013

--- apps/admin/src/components/common/DeliveryOptionsExcerptCarrierName.vue
+++ apps/admin/src/components/common/DeliveryOptionsExcerptCarrierName.vue
@@ -8,14 +8,14 @@
 
 <script lang="ts" setup>
 import {computed, toRefs, toValue} from 'vue';
-import {type Carrier} from '@myparcel-dev/pdk-common';
+import {type CarrierModel} from '@myparcel-dev/pdk-common';
 import {useFetchCarrier} from '../../sdk';
 import CarrierLogo from './CarrierLogo.vue';
 
-const props = defineProps<{carrier: Carrier.ModelCarrier}>();
+const props = defineProps<{carrier: CarrierModel}>();
 const propRefs = toRefs(props);
 
-const carriersQuery = useFetchCarrier(propRefs.carrier.value.name);
+const carriersQuery = useFetchCarrier(propRefs.carrier.value.carrier);
 
 const carrierData = computed(() => toValue(carriersQuery.data));
 </script>
