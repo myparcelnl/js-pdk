f62d5445bc60c741a60ff7ab5511acd0792542dd
Joeri van Veen <joeri@myparcel.nl>
2026-04-14 14:24:18 +0200

    feat: implement show / hide in settings screens and tabs

--- apps/admin/src/forms/pluginSettings/index.ts
+++ apps/admin/src/forms/pluginSettings/index.ts
@@ -2,5 +2,6 @@ export * from './createFormTab';
 export * from './createPluginSettingsForm';
 export * from './createPluginSettingsTabs';
 export * from './createPluginSettingsTabsComponent';
+export * from './filterPluginSettingsView';
 export * from './generateFormFields';
 export * from './types';
